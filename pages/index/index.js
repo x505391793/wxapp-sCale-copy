fd0c19e5f4a6a5355783cfb451407702df013971
xieys <505391793@qq.com>
2020-11-22 15:42:27 +0800

    upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    primarySize: "mini"
   },
   //事件处理函数
   bindViewTap: function() {
@@ -49,6 +50,11 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  toCalc: function(){
+    wx.navigateTo({
+      url: '../calc/calc',
     })
   }
 })
